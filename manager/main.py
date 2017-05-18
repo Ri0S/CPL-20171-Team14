a2159e9ee3a414ff1a30c4dcff1809a3e0174b41
Ri0S <aa248424@gmail.com>
2017-05-18 23:42:12 +0900

    manager

---
+++ manager/main.py
@@ -0,0 +1,15 @@
+from socket import*
+
+serverName = "192.168.0.2"
+serverPort = 12345
+
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.bind((serverName, 0))
+
+clientSocket.connect((serverName, serverPort))
+
+clientSocket.send()
+clientSocket.recv()
+
+clientSocket.close()
+
